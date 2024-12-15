e979f8149b8d71c9a165807b10fee4cce8246740
Łukasz Szarejko <l.szarejko@wieltongroup.com>
2024-12-15 11:43:53 +0100

    Prepare argparse

--- src/main.py
+++ src/main.py
@@ -1,27 +1,95 @@
+from argparse import ArgumentParser
+
 import torch
-from dotenv import load_dotenv
-from pyaml_env import parse_config
 
 import wandb
-from models.feature_extractor import get_pretrained_model, get_transform
-from models.utils import get_model
-
-load_dotenv()
-config = parse_config('environment.yml')
-
-wandb.require("core")
-
-wandb.init(
-    project=config["wandb"]["project-name"]
-)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main():
-    train_transform, test_transform = get_transform(config["model"]["name"])
-    feature_extractor = get_pretrained_model(config["model"]["feature_extractor"])
-    model = get_model(config["model"]["name"], feature_extractor, config["model"]["feature_extractor_dim"],
-                      config["model"]["fe_dtype"], False, config["model"]["encoder_size"])
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=config["model"]["lr"])
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        required=True,
+        choices=["CAML", "PMF", "PMF_Finetune"],
+        help="Specify the model type for experiment. Options: ['CAML', 'PMF', 'PMF_Finetune']"
+    )
+
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        required=True,
+        choices=["miniImageNet", "tieredImageNet"],
+        help="Specify the dataset for experiment. Options: ['miniImageNet', 'tieredImageNet']"
+    )
+
+    parser.add_argument(
+        "--way",
+        type=int,
+        default=5,
+        help="Specify the number of classes in a task. Default: 5"
+    )
+
+    parser.add_argument(
+        "--shot",
+        type=int,
+        default=5,
+        help="Specify the number of support examples per class. Default: 5"
+    )
+
+    parser.add_argument(
+        "--number_of_tasks",
+        type=int,
+        default=20,
+        help="Specify the number of tasks for training and evaluation. Default: 20"
+    )
+
+    parser.add_argument(
+        "--feature_extractor",
+        type=str,
+        default="timm/vit_small_patch16_224.dino",
+        help="Specify the feature extractor for the model. Default: 'timm/vit_small_patch16_224.dino'"
+    )
+
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        help="Specify the number of epochs for training. If 0, only evaluation is performed. Default: 10"
+    )
+
+    parser.add_argument(
+        "--encoder_size",
+        type=str,
+        default="tiny",
+        choices=["tiny", "small", "base", "large", "convnext", "laion", "resnet34", "huge"],
+        help="Specify the size of the encoder in CAML model. Options: ['tiny', 'small', 'base', 'large', 'convnext', 'laion', 'resnet34', 'huge']. Default: 'tiny'"
+    )
+
+    parser.add_argument(
+        "--use_wandb",
+        action="store_true",
+        help="Enable logging to Weights & Biases."
+    )
+
+    parser.add_argument(
+        "--wandb_project",
+        type=str,
+        default="few-shot-learning",
+        help="Specify the Weights & Biases project name. Default: 'few-shot-learning'"
+    )
+
+    parser.add_argument(
+        "--disable_cuda",
+        action="store_true",
+        help="Disable CUDA."
+    )
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
